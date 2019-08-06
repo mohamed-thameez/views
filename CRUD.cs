4d1f828b0ee415acfefea5a510c10334609fa482
Mohamed Thameez R <mohamedthameez.r@sella.it>
2019-08-06 17:25:14 +0530

    crud

---
+++ CRUD.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CRUD<D,C> where D : DbContext where C : class
+    {
+        private D _db;
+        public CRUD(D db)
+        {
+            _db = db;
+        }
+
+        public async Task<IQueryable<C>> GetAll()
+        {
+            try {
+                return _db.Set<C>().AsNoTracking();
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<C> GetById<U>(U id)
+        {
+            try
+            {
+                return await _db.Set<C>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<int> Create(C model)
+        {
+            try
+            {
+                _db.Set<C>().Add(model);
+                return await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<int> Update<U>(C model, U id)
+        {
+            int result = 0;
+            try
+            {
+                var exist = await _db.Set<C>().FindAsync(id);
+                if (exist != null)
+                {
+                    _db.Entry(exist).CurrentValues.SetValues(model);
+                    return await _db.SaveChangesAsync();
+                }
+                else
+                    return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<int> Delete(C model) 
+        {
+            try {
+                _db.Set<C>().Remove(model);
+                return await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ EmployeeApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeApiController : ApiController
+    {
+        private static EmployeeContext db = new EmployeeContext();
+
+        CRUD<EmployeeContext, Employee> crud = new CRUD<EmployeeContext, Employee>(db); 
+
+        // GET: api/EmployeeApi
+        public IEnumerable<string> Get()
+        {
+            var lst = crud.GetAll();
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/EmployeeApi/5
+        public string Get(string id)
+        {
+            var res = crud.GetById<string>(id);
+            return "value";
+        }
+
+        // POST: api/EmployeeApi
+        public void Post([FromBody]string value)
+        {
+            Employee emp = new Employee() { Name = value, CreatedDate = DateTime.Now, CreatedBy = "test", Department = "IT", IsAdmin = false };
+            var res = crud.Create(emp);
+        }
+
+        // PUT: api/EmployeeApi/5
+        public void Put(string id, [FromBody]string value)
+        {
+            Employee emp = new Employee() { EmpId = Convert.ToString(id), Name = value, CreatedDate = DateTime.Now, CreatedBy = "test", Department = "IT", IsAdmin = false };
+            var res = crud.Update<string>(emp,Convert.ToString(id));
+        }
+
+        // DELETE: api/EmployeeApi/5
+        public void Delete(string id)
+        {
+            var value = Get(id);
+            Employee emp = new Employee() { Name = value, CreatedDate = DateTime.Now, CreatedBy = "test", Department = "IT", IsAdmin = false };
+            var res = crud.Delete(emp);
+        }
+    }
+}
